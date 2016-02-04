68f063effddce94107a7028cd01448260afe3e0b
Velenir <Velenir@users.noreply.github.com>
2016-02-04 22:23:32 +0300

    added login and register routes

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// Used to connect to the MongoDB database
-var mongo = require('mongodb')
+//passport requirements
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -23,10 +25,32 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+//use express-session before passport.session()
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+//use passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// passport config
+var Account = require('./models/account');
+// passport.use(new LocalStrategy(Account.authenticate()));
+passport.use(Account.createStrategy()); //better
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose establish connection to db
+mongoose.connect('mongodb://localhost/passport_local_mongoose_express4');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,99 @@
 var express = require('express');
+
+//require passport
+var passport = require('passport');
+var Account = require('../models/account');
+
 var router = express.Router();
-var mongodb = require('mongodb');
+
+// if not logged in go to '/login_register'
+function requireAuthorization(req, res, next) {
+  if(req.user){
+    return next();
+  }
+  // remember from where was redirected
+
+  res.render('login_register', {redirectedFrom: req.baseUrl + req.originalUrl, requrl: req.url, originalUrl: req.originalUrl, baseUrl: req.baseUrl, reqPath: req.path, routePath: router.path()});
+}
+
+// if already logged in don't go to /register or /login
+function alreadyLoggedIn(req, res, next) {
+  if(req.user){
+    return res.redirect('/');
+  }
+  next();
+}
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.render('index', { user : req.user });
 });
+
+// when accessing /register or /login when already logged in rediret to '/'
+router.all(['/register', '/login', '/login_register'], alreadyLoggedIn);
+
+// Get register page
+router.get('/register', function(req, res) {
+    res.render('register', { });
+});
+
+// On Register form submission
+router.post('/register', function(req, res) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+          return res.render("register", {info: "Sorry. That username already exists. Try again."});
+        }
+
+        passport.authenticate('local')(req, res, function () {
+          if(req.body.redirectedFrom)
+          // redirect back from whence user came
+            res.redirect(req.body.redirectedFrom);
+          else
+            res.redirect('/');
+        });
+    });
+});
+
+
+
+// Get Login page
+router.get('/login', function(req, res) {
+    res.render('login', { user : req.user });
+});
+
+// On Login form submission
+router.post('/login', function(req, res, next) {
+    // try to authenticate
+  passport.authenticate('local', function(err, user, info) {
+    //   propagate any exceptions
+    if (err) { return next(err); }
+    //  if authentication failed
+    if (!user) {
+      return res.render('login', {info: info});
+    }
+    //  if authentication succeeded establish a user session
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      // redirect back from whence user came
+      if(req.body.redirectedFrom) return res.redirect(req.body.redirectedFrom);
+
+      return res.redirect('/');
+    //   return res.redirect('/users/' + user.username);
+    });
+  })(req, res, next);
+});
+
+// Logout
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+
+
+// routes other than above only accessible to logged-in users
+router.all('/*', requireAuthorization);
 
 // GET post assignment page
 router.get('/week:id/post', function(req, res, next) {
